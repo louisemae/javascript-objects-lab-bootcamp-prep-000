c0d28f0f8549d76abc6a8b01a86c391e8efbe389
Louise Johnson <louisemaejohnson@gmail.com>
2018-01-11 03:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object[key];
+  objectCopy = object;
+  delete objectCopy[key];
+  return objectCopy;
 }
